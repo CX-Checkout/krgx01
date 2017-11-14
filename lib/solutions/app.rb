3edaed2c2738ddf99a0cb19f83c5d34fbf8d619a
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-14 15:16:26 +0000

    Tue Nov 14 15:16:26 GMT 2017

--- lib/solutions/app.rb
+++ lib/solutions/app.rb
@@ -6,35 +6,35 @@ end
 
 class Checkout
 
-    PRICE_TABLE = {
-        'A' => 50,
-        'B' => 30,
-        'C' => 20,
-        'D' => 15
-    }
+  PRICE_TABLE = {
+      'A' => 50,
+      'B' => 30,
+      'C' => 20,
+      'D' => 15
+  }
 
-    def initialize(skus)
-      @items = skus.split('')
-    end
+  def initialize(skus)
+    @items = skus.split('')
+  end
 
-    def total
+  def total
     return -1 unless items_valid?
     subtotal - discounts
-    end
+  end
 
   def items_valid?
-    @items.all? { |item| PRICE_TABLE.keys.include?(item) }
+    @items.all? {|item| PRICE_TABLE.keys.include?(item)}
   end
 
   def subtotal
-    @items.map { |item| PRICE_TABLE[item] }
-    .reduce(0, :+)
+    @items.map {|item| PRICE_TABLE[item]}
+        .reduce(0, :+)
   end
 
   def discounts
     total_discounts = 0
-    a_discounts = @items.select { |i| i == 'A' }.each_slice(3).select { |x| x.length == 3}.count
-    b_discounts = @items.select { |i| i == 'B' }.each_slice(2).select { |x| x.length == 2}.count
+    a_discounts = @items.select {|i| i == 'A'}.each_slice(3).select {|x| x.length == 3}.count
+    b_discounts = @items.select {|i| i == 'B'}.each_slice(2).select {|x| x.length == 2}.count
     total_discounts += 20 * a_discounts
     total_discounts += 15 * b_discounts
     total_discounts
