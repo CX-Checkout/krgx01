dbda7b6f469a8cd734c61d24e960da5a8fbe8fa8
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-14 15:42:55 +0000

    Tue Nov 14 15:42:55 GMT 2017

--- test/group_discount_offer_test.rb
+++ test/group_discount_offer_test.rb
@@ -44,36 +44,36 @@ class GroupDiscountOfferTest < Minitest::Test
     assert_equal 12, offer.apply({'S' => 1, 'T' => 1, 'X' => 1}, PRICE_TABLE), 'Reduces price to equivalent of 45'
   end
 
-  # def test_more_expensive_item
-  #   offer = GroupDiscountOffer.new('STXYZ', 45, 3)
-  #   assert_equal 16, offer.apply({'S' => 1, 'T' => 1, 'Y' => 1}, PRICE_TABLE), 'Reduces price to equivalent of 45'
-  # end
-  #
-  # def test_offer_give_best_price
-  #   offer = GroupDiscountOffer.new('STXYZ', 45, 3)
-  #   assert_equal 16, offer.apply({'S' => 1, 'T' => 1, 'Y' => 1, 'X' => 1, 'Z' => 1}, PRICE_TABLE), 'Reduces price to equivalent of 45'
-  # end
-  #
-  # def test_group_multibuy_stx
-  #   offer = GroupDiscountOffer.new('STXYZ', 45, 3)
-  #   assert_equal 12, offer.apply({'S' => 1, 'T' => 1, 'Y' => 1}, PRICE_TABLE)
-  # end
-  #
-  # def test_group_multibuy_stxstx
-  #   offer = GroupDiscountOffer.new('STXYZ', 45, 3)
-  #   assert_equal 24, offer.apply({'S' => 2, 'T' => 2, 'Y' => 2}, PRICE_TABLE)
-  # end
-  #
-  # def test_group_multibuy_sss
-  #   offer = GroupDiscountOffer.new('STXYZ', 45, 3)
-  #   assert_equal 15, offer.apply({'S' => 3}, PRICE_TABLE)
-  # end
-  #
-  #
-  # def test_offer_give_lots_of_combinations
-  #   offer = GroupDiscountOffer.new('STXYZ', 45, 3)
-  #   assert_equal 45, offer.apply({'S' => 1, 'T' => 1, 'Y' => 3, 'X' => 1, 'Z' => 3}, PRICE_TABLE), 'Reduces price to equivalent of 45'
-  # end
+  def test_more_expensive_item
+    offer = GroupDiscountOffer.new('STXYZ', 45, 3)
+    assert_equal 16, offer.apply({'S' => 1, 'T' => 1, 'Y' => 1}, PRICE_TABLE), 'Reduces price to equivalent of 45'
+  end
+
+  def test_offer_give_best_price
+    offer = GroupDiscountOffer.new('STXYZ', 45, 3)
+    assert_equal 16, offer.apply({'S' => 1, 'T' => 1, 'Y' => 1, 'X' => 1, 'Z' => 1}, PRICE_TABLE), 'Reduces price to equivalent of 45'
+  end
+
+  def test_group_multibuy_stx
+    offer = GroupDiscountOffer.new('STXYZ', 45, 3)
+    assert_equal 12, offer.apply({'S' => 1, 'T' => 1, 'Y' => 1}, PRICE_TABLE)
+  end
+
+  def test_group_multibuy_stxstx
+    offer = GroupDiscountOffer.new('STXYZ', 45, 3)
+    assert_equal 24, offer.apply({'S' => 2, 'T' => 2, 'Y' => 2}, PRICE_TABLE)
+  end
+
+  def test_group_multibuy_sss
+    offer = GroupDiscountOffer.new('STXYZ', 45, 3)
+    assert_equal 15, offer.apply({'S' => 3}, PRICE_TABLE)
+  end
+
+
+  def test_offer_give_lots_of_combinations
+    offer = GroupDiscountOffer.new('STXYZ', 45, 3)
+    assert_equal 45, offer.apply({'S' => 1, 'T' => 1, 'Y' => 3, 'X' => 1, 'Z' => 3}, PRICE_TABLE), 'Reduces price to equivalent of 45'
+  end
 
 
 end
